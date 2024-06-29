dff5f428002b47a2247bc5f738a25e39c74741af
Hailey <me@haileyok.com>
2024-06-28 17:43:31 -0700

    clean

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
+interface Params {
+  modelName: string
+}
+
 class Index {
+  constructor({}) {}
 
+  run() {}
+}
 
-  constructor() {
-
-  }
+function log(message: string) {
+  const date = new Date().toISOString()
+  console.log(`[LOG: ${date}] ${message}`)
 }
